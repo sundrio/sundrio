79edcd6c44570e07f4465735d72c60d91c4bfc03
Ioannis Canellos <iocanel@gmail.com>
2017-01-10 14:50:42 +0200

    Add @Resourcecify annotation and processor for easily marking sourcefiles to copy as resources.

--- codegen/src/main/java/io/sundr/codegen/utils/IOUtils.java
+++ codegen/src/main/java/io/sundr/codegen/utils/IOUtils.java
@@ -18,9 +18,17 @@ package io.sundr.codegen.utils;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.tools.FileObject;
 
 public class IOUtils {
 
+    /**
+     * Closes multiple {@link Closeable} objects swallowing exceptions.
+     * @param cloasebales   The {@link Closeable} objects.
+     */
     public static void closeQuietly(Closeable... cloasebales) {
         if (cloasebales != null) {
             for (Closeable c : cloasebales) {
@@ -32,6 +40,29 @@ public class IOUtils {
                     //ignore
                 }
             }
+        }
+    }
+
+
+    /**
+     * Copy one {@link FileObject} into an other.
+     * @param source    The source {@link FileObject}.
+     * @param target    The target {@link FileObject}.
+     * @throws IOException
+     */
+    public static void copy(FileObject source, FileObject target) throws IOException {
+        InputStream in = source.openInputStream();
+        OutputStream out = target.openOutputStream();
+        try {
+            byte[] buffer = new byte[1024];
+            int len = in.read(buffer);
+            while (len != -1) {
+                out.write(buffer, 0, len);
+                len = in.read(buffer);
+            }
+        } finally {
+            in.close();
+            out.close();
         }
     }
 }
