a5d283773be70ace4ba0e7e53a3ab37b57ff1eae
Ioannis Canellos <iocanel@gmail.com>
2020-12-21 12:08:28 +0200

    feat: Improve identation of generated code.

--- codegen/src/main/java/io/sundr/codegen/directives/MethodDirective.java
+++ codegen/src/main/java/io/sundr/codegen/directives/MethodDirective.java
@@ -26,6 +26,10 @@ import org.apache.velocity.runtime.parser.node.Node;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class MethodDirective extends Directive {
 
@@ -44,6 +48,7 @@ public class MethodDirective extends Directive {
         String block = "";
         Method method = null;
         Boolean isInterface = false;
+        int level = 1;
         for (int i = 0; i < node.jjtGetNumChildren(); i++) {
             if (node.jjtGetChild(i) != null) {
                 if (!(node.jjtGetChild(i) instanceof ASTBlock)) {
@@ -52,6 +57,8 @@ public class MethodDirective extends Directive {
                         method = (Method) node.jjtGetChild(i).value(context);
                     } else if (i == 1) {
                         isInterface = (Boolean) node.jjtGetChild(i).value(context);
+                    } else if (i == 2) {
+                        level = (Integer) node.jjtGetChild(i).value(context);
                     } else {
                         break;
                     }
@@ -66,18 +73,66 @@ public class MethodDirective extends Directive {
             }
         }
         boolean hasBody = !method.isAbstract() && (!isInterface || method.isDefaultMethod());
-        writeMethod(writer, method, block, hasBody);
+        writeMethod(writer, method, block, hasBody, level);
         return true;
     }
 
-    private void writeMethod(Writer writer, Method method, String block, Boolean hasBody) throws IOException {
+  private void writeMethod(Writer writer, Method method, String block, Boolean hasBody, int level) throws IOException {
         if (method != null) {
-            writer.append(method.toString());
+          String onetab  = tab(level * 4);
+          String twotabs = tab((level + 1) * 4);
+          writer.append(onetab).append(method.toString());
+            List<String> lines = getLines(block);
+            List<String> indentedLines = lines.stream().map(l -> twotabs + l).collect(Collectors.toList());
             if (hasBody) {
-                writer.append(block);
+              writer.append(" {").append(System.lineSeparator());
+              for (String line : indentedLines) {
+                writer.append(line).append(System.lineSeparator());
+              }
+              writer.append(onetab).append("}").append(System.lineSeparator());
             } else {
                 writer.append(";");
             }
         }
     }
+
+  private static List<String> getLines(String block) {
+    List<String> lines = Arrays.stream(block.split("\r|\n")).collect(Collectors.toList());
+    return lines;
+  }
+  
+  private static int getIndentationLevel(List<String> lines) {
+    TreeSet<Integer> sizes = new TreeSet(lines.stream().map(MethodDirective::level).sorted().collect(Collectors.toSet()));
+    int min = sizes.first();
+    int max = sizes.last();
+    if (Math.abs(max - min) % 4 == 0) {
+      return 4;
+    } else return 2;
+  }
+
+  private static int getIndentationBase(List<String> lines) {
+    TreeSet<Integer> sizes = new TreeSet<>(lines.stream().map(String::length).sorted().collect(Collectors.toSet()));
+    int min = sizes.first();
+    return min;
+  }
+
+  private static int level(String line) {
+    int level=0;
+    for (int i = 0; i < line.length(); i++) {
+      if (line.charAt(i) == ' ') {
+        level++;
+      } else {
+        return level;
+      }
+    }
+    return level;
+  }
+  
+  private static String tab(int level) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < level; i++) {
+      sb.append(" ");
+    }
+    return sb.toString();
+  }
 }
