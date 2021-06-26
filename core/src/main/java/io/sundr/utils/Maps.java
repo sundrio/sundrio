c407e126f6347aee8bc84ff56becfc07ae0f2da5
Ioannis Canellos <iocanel@gmail.com>
2021-06-26 12:56:58 +0300

    feat: Add util to convert comma separate [key]=[value] strings to Maps

--- core/src/main/java/io/sundr/utils/Maps.java
+++ core/src/main/java/io/sundr/utils/Maps.java
@@ -17,13 +17,54 @@
 
 package io.sundr.utils;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public final class Maps {
 
+  private static final String EQUAL = "=";
+  private static final String COMMA_PATTERN = ",[ ]*";
+  private static final String ILLEGAL_FORMAT = "Illegal format. Required: <key>=<value>";
+
   private Maps() {
     //Utility class
+  }
+
+  /**
+   * Extract the key from the mapping
+   * 
+   * @param mapping the mapping ([key]=[value])
+   * @return the key or throws an exception if the mapping format is invalid
+   */
+  public static String extractKey(String mapping) {
+    if (Strings.isNullOrEmpty(mapping) || !mapping.contains(EQUAL)) {
+      throw new IllegalArgumentException(ILLEGAL_FORMAT);
+    }
+    return mapping.substring(0, mapping.indexOf(EQUAL)).trim();
+  }
+
+  /**
+   * Extract the value from the mapping
+   * 
+   * @param mapping the mapping ([key]=[value])
+   * @return the key or throws an exception if the mapping format is invalid
+   */
+  public static String extractValue(String mapping) {
+    if (Strings.isNullOrEmpty(mapping) || !mapping.contains(EQUAL)) {
+      throw new IllegalArgumentException(ILLEGAL_FORMAT);
+    }
+    return mapping.substring(mapping.indexOf(EQUAL) + 1).trim();
+  }
+
+  public static Map<String, String> create(String mappings) {
+    if (Strings.isNullOrEmpty(mappings)) {
+      return Collections.emptyMap();
+    }
+    return Arrays.stream(mappings.split(COMMA_PATTERN)).filter(Strings::isNotNullOrEmpty)
+        .collect(Collectors.toMap(Maps::extractKey, Maps::extractValue));
   }
 
   public static <K, V> Map<K, V> create(K key, V value) {
@@ -46,4 +87,5 @@ public final class Maps {
     result.put(key3, value3);
     return result;
   }
+
 }
