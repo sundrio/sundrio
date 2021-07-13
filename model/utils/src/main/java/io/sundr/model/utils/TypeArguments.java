26acc8afd8af8ba937e6b3545e7cf8dad267fc76
Ioannis Canellos <iocanel@gmail.com>
2021-07-13 15:04:38 +0300

    feat: TypeArguments function now popullates type mapping related attributes

--- model/utils/src/main/java/io/sundr/model/utils/TypeArguments.java
+++ model/utils/src/main/java/io/sundr/model/utils/TypeArguments.java
@@ -24,6 +24,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import io.sundr.model.AttributeKey;
 import io.sundr.model.ClassRef;
 import io.sundr.model.Method;
 import io.sundr.model.Property;
@@ -31,6 +32,7 @@ import io.sundr.model.RichTypeDef;
 import io.sundr.model.TypeDef;
 import io.sundr.model.TypeDefBuilder;
 import io.sundr.model.TypeParamDef;
+import io.sundr.model.TypeParamRef;
 import io.sundr.model.TypeRef;
 import io.sundr.model.functions.GetDefinition;
 import io.sundr.model.visitors.ApplyTypeParamMappingToMethod;
@@ -39,6 +41,9 @@ import io.sundr.model.visitors.ApplyTypeParamMappingToTypeArguments;
 import io.sundr.utils.Predicates;
 
 public class TypeArguments {
+
+  public static final AttributeKey<TypeParamRef> ORIGINAL_TYPE_PARAMETER = new AttributeKey<>("ORIGINAL_TYPE_PARAMETER",
+      String.class);
 
   private static final Predicate<ClassRef> INTERNAL_JDK = c -> c.equals(Types.OBJECT_REF)
       || c.getFullyQualifiedName().startsWith("jdk.internal");
@@ -80,19 +85,18 @@ public class TypeArguments {
     }
 
     return new TypeDefBuilder(definition)
-        .accept(new ApplyTypeParamMappingToProperty(mappings),
-            new ApplyTypeParamMappingToMethod(mappings),
+        .accept(new ApplyTypeParamMappingToProperty(mappings, ORIGINAL_TYPE_PARAMETER),
+            new ApplyTypeParamMappingToMethod(mappings, ORIGINAL_TYPE_PARAMETER),
             new ApplyTypeParamMappingToTypeArguments(mappings))
         .build();
   }
 
   private static List<Property> applyToProperties(TypeDef definition) {
     return Stream
-        .concat(definition.getProperties().stream(), definition.getExtendsList().stream()
-            .filter(INTERNAL_JDK.negate())
-            .flatMap(e -> applyToProperties(apply(e)).stream()))
-        .filter(Predicates.distinct(Property::withErasure))
-        .collect(Collectors.toList());
+        .concat(definition.getProperties().stream(),
+            definition.getExtendsList().stream().filter(INTERNAL_JDK.negate())
+                .flatMap(e -> applyToProperties(apply(e)).stream()))
+        .filter(Predicates.distinct(Property::withErasure)).collect(Collectors.toList());
   }
 
   private static List<Method> applyToMethods(TypeDef definition) {
