d62273447f62e9ca455a98818bcd4142c0efe100
Ioannis Canellos <iocanel@gmail.com>
2021-07-22 12:36:21 +0300

    feat: add support for jdk16 records

---
+++ model/utils/src/main/java/io/sundr/model/utils/Record.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2015 The original authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+**/
+
+package io.sundr.model.utils;
+
+import io.sundr.model.Kind;
+import io.sundr.model.TypeDef;
+
+public class Record {
+
+  private static final String JAVA_LANG_RECORD = "java.lang.Record";
+
+  public static boolean is(TypeDef typeDef) {
+    return typeDef.getKind() == Kind.CLASS
+        && typeDef.getExtendsList().stream().anyMatch(e -> e.getFullyQualifiedName().equals(JAVA_LANG_RECORD));
+  }
+}
