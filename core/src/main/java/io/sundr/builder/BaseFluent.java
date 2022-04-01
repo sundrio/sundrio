fb8ded27c7ac7cc270ce491122beab532c9a7988
Ioannis Canellos <iocanel@gmail.com>
2022-04-01 18:01:47 +0300

    fix: handling of buildable sets

--- core/src/main/java/io/sundr/builder/BaseFluent.java
+++ core/src/main/java/io/sundr/builder/BaseFluent.java
@@ -51,8 +51,8 @@ public class BaseFluent<F extends Fluent<F>> implements Fluent<F>, Visitable<F> 
     return list == null ? null : new ArrayList<T>(list.stream().map(Builder::build).collect(Collectors.toList()));
   }
 
-  public static <T> List<T> build(Set<? extends Builder<? extends T>> set) {
-    return set == null ? null : new ArrayList<T>(set.stream().map(Builder::build).collect(Collectors.toList()));
+  public static <T> Set<T> build(Set<? extends Builder<? extends T>> set) {
+    return set == null ? null : new LinkedHashSet<T>(set.stream().map(Builder::build).collect(Collectors.toSet()));
   }
 
   public static <T> List<T> aggregate(List<? extends T>... lists) {
