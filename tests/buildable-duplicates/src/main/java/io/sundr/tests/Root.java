513eb4014287aa38d15f074f44f1c585d9fcf18c
Marc Nuri <marc@marcnuri.com>
2024-09-02 12:55:11 +0200

    test: (disabled)reproducer test for duplicate builder methods fluent
    Reproducer test for #170.
    It's disabled at the pom level (integration test).
    
    This kind of naming generates duplicate builder methods and class nested classes.
    
    A slight change in the name of the classes (Object or ObjectSpec) prevent the issue from happening.

---
+++ tests/buildable-duplicates/src/main/java/io/sundr/tests/Root.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2015 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+**/
+
+package io.sundr.tests;
+
+import io.sundr.builder.annotations.Buildable;
+
+/**
+ * This class is the Root class where the buildable processing starts.
+ */
+@Buildable(editableEnabled = false, validationEnabled = false, generateBuilderPackage = true, lazyCollectionInitEnabled = false, builderPackage = "io.sundr.tests.builder")
+public class Root implements Resource {
+
+  private final Resource spec;
+  private final Resource status;
+
+  public Root(Resource spec, Resource status) {
+    this.spec = spec;
+    this.status = status;
+  }
+
+  public Resource getSpec() {
+    return spec;
+  }
+
+  public Resource getStatus() {
+    return status;
+  }
+}
