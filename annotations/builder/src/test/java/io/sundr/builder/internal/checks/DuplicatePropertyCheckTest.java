eeff054b3bc79d3d16376f34a4e152f32e054c9e
Ioannis Canellos <iocanel@gmail.com>
2024-11-25 12:51:39 +0200

    fix: enable annotation processing

--- annotations/builder/src/test/java/io/sundr/builder/internal/checks/DuplicatePropertyCheckTest.java
+++ annotations/builder/src/test/java/io/sundr/builder/internal/checks/DuplicatePropertyCheckTest.java
@@ -29,7 +29,7 @@ public class DuplicatePropertyCheckTest {
         .accept(new DuplicatePropertyCheck())
         .build();
   }
-  
+
   @Test
   public void shouldNotThrowExceptionWithEnum() {
     new TypeDefBuilder()
