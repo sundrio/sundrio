bfd6516f424f25b87efa3892a527947b6dda14fb
Ioannis Canellos <iocanel@gmail.com>
2025-08-29 10:12:55 +0300

    chore: make For more fluent

--- model/base/src/main/java/io/sundr/model/For.java
+++ model/base/src/main/java/io/sundr/model/For.java
@@ -56,4 +56,98 @@ public class For implements Statement {
     sb.append(CB).append(NEWLINE);
     return sb.toString();
   }
+
+  //
+  // DSL
+  //
+
+  public static DslInit init(Property prop, Object expression) {
+    return new DslInit(Arrays.asList(new Declare(prop, expression)));
+  }
+
+  public static DslInit init(Expression... init) {
+    return new DslInit(Arrays.asList(init));
+  }
+
+  public static DslInit init(List<Expression> init) {
+    return new DslInit(init);
+  }
+
+  public static class DslInit {
+    private List<Expression> init;
+
+    DslInit(List<Expression> init) {
+      this.init = init;
+    }
+
+    public DslCompareStep compare(Expression compare) {
+      return new DslCompareStep(init, compare);
+    }
+
+    public DslCompareStep eq(Expression left, Expression right) {
+      return new DslCompareStep(init, Expression.eq(left, right));
+    }
+
+    public DslCompareStep ge(Expression left, Expression right) {
+      return new DslCompareStep(init, new GreaterThanOrEqual(left, right));
+    }
+
+    public DslCompareStep gt(Expression left, Expression right) {
+      return new DslCompareStep(init, new GreaterThan(left, right));
+    }
+
+    public DslCompareStep le(Expression left, Expression right) {
+      return new DslCompareStep(init, new LessThanOrEqual(left, right));
+    }
+
+    public DslCompareStep lt(Expression left, Expression right) {
+      return new DslCompareStep(init, new LessThan(left, right));
+    }
+
+    public DslCompareStep ne(Expression left, Expression right) {
+      return new DslCompareStep(init, Expression.ne(left, right));
+    }
+
+    public DslCompareStep isNull(Expression expr) {
+      return new DslCompareStep(init, Expression.isNull(expr));
+    }
+
+    public DslCompareStep notNull(Expression expr) {
+      return new DslCompareStep(init, Expression.notNull(expr));
+    }
+  }
+
+  public static class DslCompareStep {
+    private List<Expression> init;
+    private Expression compare;
+
+    DslCompareStep(List<Expression> init, Expression compare) {
+      this.init = init;
+      this.compare = compare;
+    }
+
+    public DslBodyStep update(Expression... update) {
+      return new DslBodyStep(init, compare, Arrays.asList(update));
+    }
+
+    public DslBodyStep update(List<Expression> update) {
+      return new DslBodyStep(init, compare, update);
+    }
+  }
+
+  public static class DslBodyStep {
+    private List<Expression> init;
+    private Expression compare;
+    private List<Expression> update;
+
+    DslBodyStep(List<Expression> init, Expression compare, List<Expression> update) {
+      this.init = init;
+      this.compare = compare;
+      this.update = update;
+    }
+
+    public For body(Statement... statements) {
+      return new For(init, compare, update, Block.wrap(statements));
+    }
+  }
 }
