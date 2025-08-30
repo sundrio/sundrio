795caf2144e962dcdc902ae60cf90d025d34c890
Ioannis Canellos <iocanel@gmail.com>
2025-08-30 11:35:17 +0300

    chore: provide static method to create this references

--- model/base/src/main/java/io/sundr/model/This.java
+++ model/base/src/main/java/io/sundr/model/This.java
@@ -2,6 +2,14 @@ package io.sundr.model;
 
 public class This implements Expression {
 
+  public static PropertyRef ref(String name) {
+    return new PropertyRef(name, new This());
+  }
+
+  public static PropertyRef ref(Property property) {
+    return new PropertyRef(property, new This());
+  }
+
   @Override
   public String render() {
     return "this";
